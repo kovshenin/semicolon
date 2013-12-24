875511a162cae6990ca0c60292ec402e8eea40a9
Konstantin Kovshenin <kovshenin@gmail.com>
2013-12-24 23:40:54 +0400

    Is active sidebar or not

--- sidebar.php
+++ sidebar.php
@@ -6,13 +6,23 @@
  */
 ?>
 	<div id="secondary" class="widget-area" role="complementary">
+
+		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 		<div class="sidebar-primary">
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
 		</div>
+		<?php endif; ?>
+
+		<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
 		<div class="sidebar-secondary">
 			<?php dynamic_sidebar( 'sidebar-2' ); ?>
 		</div>
+		<?php endif; ?>
+
+		<?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
 		<div class="sidebar-tertiary">
 			<?php dynamic_sidebar( 'sidebar-3' ); ?>
 		</div>
+		<?php endif; ?>
+
 	</div><!-- #secondary -->
