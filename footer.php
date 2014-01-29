3fced36a620a3d566f7a4ee085bb62f72d288fc8
Konstantin Kovshenin <kovshenin@gmail.com>
2014-01-29 23:50:00 +0400

    More unmag

--- footer.php
+++ footer.php
@@ -16,7 +16,6 @@
 			<?php /*<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'kovkov' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span> */ ?>
 			<?php /* printf( __( 'Theme: %1$s by %2$s.', 'kovkov' ), 'kovkov', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); */ ?>
-			&copy; <?php echo date( 'Y' ); ?> Копирование материалов без разрешения автора запрещено. WordPress и WordCamp являются зарегистрированными торговыми марками и принадлежат фонду <a href="http://wordpressfoundation.org/" target="_blank">WordPress Foundation</a>. Читайте правила использования торговых марок. Работает на <a href="http://wordpress.org/" target="_blank">WordPress</a>.
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
