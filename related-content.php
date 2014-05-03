3bacacd0d356217fc885c1c1ba6be65bb4fdfc13
Konstantin Kovshenin <kovshenin@gmail.com>
2014-05-03 11:16:56 +0400

    Show related posts only if 2 or more related posts found.

--- related-content.php
+++ related-content.php
@@ -4,7 +4,7 @@
 do_action( 'semicolon_related_posts_before' );
 $related_posts = Semicolon::get_related_posts();
 ?>
-<?php if ( $related_posts->have_posts() ) : ?>
+<?php if ( $related_posts->have_posts() && $related_posts->found_posts >= 2 ) : ?>
 <div class="related-content">
 	<h3 class="related-content-title"><?php _e( 'Related posts', 'semicolon' ); ?></h3>
 
