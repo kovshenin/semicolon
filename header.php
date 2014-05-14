35bf9e4f4466ad244ddeb86b81d16dd4c8ed89cc
Konstantin Kovshenin <kovshenin@gmail.com>
2014-05-14 13:21:59 +0400

    WP Magazine styles for social.

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class( 'wpmag' ); ?>>
 <script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
 <div id="page" class="hfeed site">
 
