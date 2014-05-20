f4b55592faed29257019c2ee5d28762f4f35da9c
Konstantin Kovshenin <kovshenin@gmail.com>
2014-05-20 21:36:03 +0400

    Add author and timestamps to pages.

--- content-page.php
+++ content-page.php
@@ -18,4 +18,14 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
+
+	<footer>
+		<div class="author vcard">
+			<?php
+				$time_string = sprintf( '<time class="entry-date published" datetime="%s">%s</time>', get_the_date('c'), get_the_time( get_option( 'date_format' ) ) );
+				$time_string .= sprintf( '<time class="updated" datetime="%s">%s</time>', get_the_modified_date('c'), get_the_modified_date() );
+			?>
+			<a class="url fn n" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"><?php the_author(); ?></a> <?php echo $time_string; ?>
+		</div>
+	</footer>
 </article><!-- #post-## -->
