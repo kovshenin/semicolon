2d21262e03402e99cf69e27095b524e046cd73fd
Konstantin Kovshenin <kovshenin@gmail.com>
2014-06-17 21:48:36 +0400

    Remove empty customizer.php file.

--- functions.php
+++ functions.php
@@ -504,8 +504,3 @@ Semicolon::setup();
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
